d0868a6b756c2cc71c0125efb1a912b0691617ad
zmar234 <zachmarino234@gmail.com>
2026-05-12 21:16:59 -0400

    Basic dice roll tracker implementation

---
+++ src/components/button.svelte
@@ -0,0 +1,18 @@
+<script>
+  let { text } = $props();
+  let count = $state(0);
+
+  function increment() {
+    count += 1;
+  }
+</script>
+
+<style>
+  /* Scoped styles: only affects this component */
+  p {
+    color: #ff3e00;
+    font-weight: bold;
+  }
+</style>
+
+<button onclick={increment}>{text} + {count}</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
+<script>
+  import Button from '../components/button.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button text="1: " />
+<Button text="2: " />
+<Button text="3: " />
+<Button text="4: " />
+<Button text="5: " />
+<Button text="6: " />
+<Button text="7: " />
+<Button text="8: " />
+<Button text="9: " />
+<Button text="10: " />
+<Button text="11: " />
+<Button text="12: " />
